091987be0b2c031a98e981bcab397b0975cb7758
fbrcrsi <fbrcrsi@gmail.com>
2015-03-02 23:06:32 +0100

    1st release

---
+++ fbrcrsi/addimagehosting/ext.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* Ads extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace fbrcrsi\addimagehosting;
+
+/**
+* Extension class for custom enable/disable/purge actions
+*
+* NOTE TO EXTENSION DEVELOPERS:
+* Normally it is not necessary to define any functions inside the ext class below.
+* The ext class may contain special (un)installation commands in the methods
+* enable_step(), disable_step() and purge_step(). As it is, these methods are defined
+* in phpbb_extension_base, which this class extends, but you can overwrite them to
+* give special instructions for those cases.
+*/
+class ext extends \phpbb\extension\base
+{
+}
